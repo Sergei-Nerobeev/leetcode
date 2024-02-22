f997eff082fc0f2d18856c910fb2251819e1a2b4
nero <sergeinerobeev@gmail.com>
2024-02-22 19:18:55 +0100

    Done

---
+++ src/Allgoritms/MissingNumber.java
@@ -0,0 +1,27 @@
+package Allgoritms;
+
+public class MissingNumber {
+    public static void main(String[] args) {
+        int[] numbers = {3, 0, 1};
+        System.out.println(MissingNumber.missingNumber(numbers));
+    }
+
+    public static int missingNumber(int[] numbers) {   //TODO оценить сложность алгоритма
+
+        for (int temp = 0; temp < numbers.length; temp++) {
+            if (!isNumberIn(numbers, temp)) {
+                return temp;
+            }
+        }
+        return numbers.length;
+    }
+
+    public static boolean isNumberIn(int[] numbers, int temp) {
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == temp) {
+                return true;
+            }
+        }
+        return false;
+    }
+}

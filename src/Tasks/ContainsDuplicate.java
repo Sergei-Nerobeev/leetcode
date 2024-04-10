8a5ac0c4be0c587a4768c534a3b7a2bfbbf489b2
nero <sergeinerobeev@gmail.com>
2024-04-10 15:52:49 +0200

    adding ContainsDuplicate

--- src/Tasks/ContainsDuplicate.java
+++ src/Tasks/ContainsDuplicate.java
@@ -2,17 +2,23 @@ package Tasks;
 
 
 import java.util.HashSet;
-import java.util.Set;
 
 public class ContainsDuplicate {
         public static void main(String[] args) {
-
+            int[] numbers = new int[] {234, 345, 678, 34, 679458, 4856, 459683};
+        boolean result = containsDuplicate(numbers);
+            System.out.println(result);
 
     }
-    public boolean containsDuplicate(int[] nums){
-        Set<Integer> searchDuplicate = new HashSet<>();
-        for (Integer i : searchDuplicate) {
-            if (set.)
+    public static boolean containsDuplicate(int[] numbs){
+        HashSet<Integer> set = new HashSet<>();
+        for (Integer i : numbs) {
+            if (set.contains(i)) {
+                return true;
+            }
+            set.add(i);
+
         }
+        return false;
     }
 }

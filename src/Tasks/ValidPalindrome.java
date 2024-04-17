94c41c84315063eb996c17b0101cda3dc0486100
nero <sergeinerobeev@gmail.com>
2024-04-17 15:55:27 +0200

    add ValidPalindrome.java

---
+++ src/Tasks/ValidPalindrome.java
@@ -0,0 +1,31 @@
+package Tasks;
+
+public class ValidPalindrome {
+    public static void main(String[] args) {
+
+        System.out.println(isPalindrome("mkmk"));
+    }
+
+    public static boolean isPalindrome(String string) {
+        int left = 0;
+        int right = string.length() - 1;
+
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(string.charAt(left))) {
+                left++;
+            }
+
+            while (left < right && !Character.isLetterOrDigit(string.charAt(right))) {
+                right--;
+            }
+
+            if (Character.toLowerCase(string.charAt(left)) != Character.toLowerCase(string.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+        return true;
+    }
+}

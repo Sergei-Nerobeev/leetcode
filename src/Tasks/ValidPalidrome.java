b234589efe1157e1e48b6a1f828df6cc009bfc9e
Sergei Nerobeev <sergeinerobeev@gmail.com>
2024-04-18 22:38:03 +0200

    done isAnagram

---
+++ src/Tasks/ValidPalidrome.java
@@ -0,0 +1,11 @@
+package Tasks;
+
+public class ValidPalidrome {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isPalindrome(String s) {
+        return true;
+    }
+}

9224243bc788077aa6ae3603f6ca043f76ec360f
Sergei Nerobeev <sergeinerobeev@gmail.com>
2024-05-12 23:24:52 +0200

    ValidPalindrome

--- src/Tasks/ValidPalindrome.java
+++ src/Tasks/ValidPalindrome.java
@@ -2,15 +2,19 @@ package Tasks;
 
 public class ValidPalindrome {
     public static void main(String[] args) {
-        String s = "A man, a plan, a canal: Panama";
-        System.out.println(isPalindrome(s));
+//        String s = "A man, a plan, a canal: Panama";
+//        System.out.println(isPalindrome(s));
+        reverse("жопа");
+//        switchInts();
     }
 
+    //казак
     public static boolean isPalindrome(String s) {
         int left = 0;
         int right = s.length() - 1;
 
         while (left < right) {
+
             while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
                 left++;
             }
@@ -28,5 +32,45 @@ public class ValidPalindrome {
         }
 
         return true;
+    }
+
+
+    public static String reverse(String word) {  // time and memory
+        char tempLetter;
+        char[] wordCharArray = word.toCharArray();
+        for (int i = 0; i < wordCharArray.length / 2; i++) {
+            // кот, получить из слова кот слово ток
+            // i = 0 (к), ожидаю i = 0 (т)
+            // i = 1 (о), ожидаю i = 1 (о)
+            // i = 2 (т), ожидаю i = 2 (к)
+            tempLetter = wordCharArray[i]; // k
+            wordCharArray[i] = wordCharArray[wordCharArray.length - 1 - i]; // t
+            wordCharArray[wordCharArray.length - 1 - i] = tempLetter; // k
+            // i = 0, ток
+            // i = 1, ток
+            // i = 2, kot
+
+        }
+        return String.valueOf(wordCharArray);
+    }
+
+    public static void switchChars() {
+        char tmp;
+        char a = 'a';
+        char b = 'b';
+        tmp = a;
+        a = b;
+        b = tmp;
+
+    }
+
+    public static void switchInts() {
+        int a = 1;
+        int b = 3;
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println(a + "" + b);
+        //поменять а и б местами, не используя временной переменной, use math operaton
     }
 }

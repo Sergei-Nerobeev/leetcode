a70bdb681032db985e8286e5385755fdaf4e6de9
nero <sergeinerobeev@gmail.com>
2024-05-16 17:20:37 +0200

    add ValidPalindrome.java

--- src/tasks/ValidPalindrome.java
+++ src/tasks/ValidPalindrome.java
@@ -2,78 +2,36 @@ package tasks;
 
 public class ValidPalindrome {
     public static void main(String[] args) {
-//        String s = "A man, a plan, a canal: Panama";
-//        System.out.println(isPalindrome(s));
-        System.out.println(issPalindrome("A man, a plan, a canal: Panama"));
+
+//        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+        System.out.println(isPalindrome("race a car"));
 
     }
-    //казак
-//    public static boolean isPalindrome(String s) {
-//        int left = 0;
-//        int right = s.length() - 1;
-//
-//        while (left < right) {
-//
-//            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
-//                left++;
-//            }
-//
-//            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
-//                right--;
-//            }
-//
-//            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
-//                return false;
-//            }
-//
-//            left++;
-//            right--;
-//        }
-//
-//        return true;
-//    }
 
-//    public static String reverse(String word) {  // time and memory
-//        char tempLetter;
-//        char[] wordCharArray = word.toCharArray();
-//        for (int i = 0; i < wordCharArray.length / 2; i++) {
-//            //N -> K
-//
-//            tempLetter = wordCharArray[i];
-//            wordCharArray[i] = wordCharArray[wordCharArray.length - 1 - i];
-//            wordCharArray[wordCharArray.length - 1 - i] = tempLetter;
-//
-//        }
-//        return String.valueOf(wordCharArray);
-//    }
-
-//    public static boolean isPalindrome(String word) {  // time and memory
-//        char[] wordCharArray = word.toUpperCase().toCharArray();
-//        int rightIndex = wordCharArray.length - 1;
-//        for (int i = 0; i < wordCharArray.length / 2; i++) {
-//            //A -> A
-//            //space -> M
-//            if (wordCharArray[i] != wordCharArray[wordCharArray.length - 1 - i]) {
-//                return false;
-//            }
-//
-//        }
-//        return true;
-//    }
-
-    public static boolean issPalindrome(String word) {
+    public static boolean isPalindrome(String word) {
         char[] wordCharArray = word.toUpperCase().toCharArray();
         int leftIndex = 0;
         int rightIndex = wordCharArray.length - 1;
         while (leftIndex < rightIndex) {
+            if (!Character.isLetterOrDigit(wordCharArray[leftIndex])) {
+                leftIndex++;
+                continue;
+            }
+            if (!Character.isLetterOrDigit(wordCharArray[rightIndex])) {
+                rightIndex--;
+                continue;
+            }
             if (wordCharArray[leftIndex] != wordCharArray[rightIndex]) {
                 return false;
             }
             leftIndex++;
             rightIndex--;
+
         }
         return true;
+
     }
+
 
     public static void switchChars() {
         char tmp;

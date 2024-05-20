257ba852e0b08b84e56a481daf2910a274c06d21
nero <sergeinerobeev@gmail.com>
2024-05-20 16:45:21 +0200

    add MaxVolumeMassive.java.java

---
+++ src/tasks/lessons/massive/task2/MaxVolumeMassive.java
@@ -0,0 +1,22 @@
+package tasks.lessons.massive.task2;
+
+public class MaxVolumeMassive {
+    public static void main(String[] args) {
+
+        int[] array = {2, 4, 6, 8, 10};
+        System.out.println(findMaxValue(array));
+
+    }
+    public static int findMaxValue(int[] array) {
+        if(array == null  || array.length == 0) {
+            throw new RuntimeException("Array is empty ");
+        }
+        int maxValue = array[0];
+        for (int index = 1; index < array.length; index++) {
+            if (array[index] > maxValue) {
+                maxValue = array[index];
+            }
+        } return maxValue;
+    }
+
+}

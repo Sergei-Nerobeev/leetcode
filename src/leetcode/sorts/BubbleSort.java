f6e53775ae6a306877d889748e97c6d110d0c48e
Sergei Nerobeev <sergeinerobeev@gmail.com>
2024-05-22 23:55:52 +0200

    add new BubbleSort

--- src/leetcode/sorts/BubbleSort.java
+++ src/leetcode/sorts/BubbleSort.java
@@ -3,5 +3,21 @@ package leetcode.sorts;
 public class BubbleSort {
     public static void main(String[] args) {
 
+        int[] arr = {2, 56, 5, 1, 33};
+        bubbleSort(arr);
+    }
+
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Меняем элементы местами
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
     }
 }
+

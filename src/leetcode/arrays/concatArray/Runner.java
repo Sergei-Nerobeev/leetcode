501f4ffac000f134f03d21f644d260d231e91025
Sergei Nerobeev <sergeinerobeev@gmail.com>
2024-06-02 23:04:27 +0200

    add concatArray

--- src/leetcode/arrays/concatArray/Runner.java
+++ src/leetcode/arrays/concatArray/Runner.java
@@ -5,9 +5,10 @@ import java.util.Arrays;
 public class Runner {
     public static void main(String[] args) {
 
-        int[] numbers = {4, 2, 6, 8};
+        int[] numbers = {4, 56, 2, 100, 45, 3, 88};
 
-        System.out.println(Arrays.toString(repeatArray(numbers)));
+//        System.out.println(Arrays.toString(repeatArray(numbers)));
+        System.out.println(Arrays.toString(halfOfArray(numbers)));
     }
 
     public static int[] repeatArray(int[] num) {
@@ -22,4 +23,26 @@ public class Runner {
         return res;
     }
 
+    //на вход метода приходит массив, на выход ожидается новый массив состоящий из половины входного
+//
+//    Пример:
+//    in = [ "dd", "d", "aa", "ww", "qq"]
+//    out = ["dd", "d"]
+
+    public static int [] halfOfArray(int[] num) {
+        int len = num.length;
+        int halfLen = len / 2;
+        int result[] = new int[halfLen];
+        for (int i = 0; i < halfLen; i++) {
+            result[i] = num[i];
+        }
+        return result;
+    }
+
+
+//    инпут - aabbba
+//    output - 2a3b1a
+    public static void archiveInfo(String letter) {
+
+    }
 }

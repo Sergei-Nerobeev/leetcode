9459319c825728a968d721df8e62b9539e2cca73
nero <sergeinerobeev@gmail.com>
2024-06-03 17:44:36 +0200

    done repeatArray with test

--- src/leetcode/arrays/concatArray/Runner.java
+++ src/leetcode/arrays/concatArray/Runner.java
@@ -5,10 +5,41 @@ import java.util.Arrays;
 public class Runner {
     public static void main(String[] args) {
 
-        int[] numbers = {4, 56, 2, 100, 45, 3, 88};
+        String[] letters = {"dd", "d", "aa", "ww", "qq"};
+        String[] correct = {"dd", "d"};
+        String[] letters2 = {"aa", "1", null, "ww", "qq"};
+        String[] correct2 = {"aa", "1"};
+        String[] letters3 = {"с", "п", "р", "и", "н", "г"};
+        String[] correct3 = {"с", "п", "р"};
+        String[] letters4 = {"single"};
+        String[] correct4 = {};
+        String[] letters5 = {null, null};
+        String[] correct5 = {null};
+        String[] letters6 = {"123", "4567", "789000"};
+        String[] correct6 = {"123"};
+        String[] letters7 = {};
+        String[] correct7 = {};
+        String[] letters8 = {null};
+        String[] correct8 = {};
 
-//        System.out.println(Arrays.toString(repeatArray(numbers)));
-        System.out.println(Arrays.toString(halfOfArray(numbers)));
+        System.out.println((Arrays.toString(halfOfStringArray(letters))));
+        halfOfStringArrayTest(letters, correct);
+        halfOfStringArrayTest(letters2, correct2);
+        halfOfStringArrayTest(letters3, correct3);
+        halfOfStringArrayTest(letters4, correct4);
+        halfOfStringArrayTest(letters5, correct5);
+        halfOfStringArrayTest(letters6, correct6);
+        halfOfStringArrayTest(letters7, correct7);
+        halfOfStringArrayTest(letters8, correct8);
+    }
+
+    public static void halfOfStringArrayTest(String[] input, String[] expected) {
+        String[] result = halfOfStringArray(input);
+        if (Arrays.equals(result, expected)) {
+            System.out.println("Test true");
+        } else {
+            System.out.println("Test false");
+        }
     }
 
     public static int[] repeatArray(int[] num) {
@@ -29,7 +60,7 @@ public class Runner {
 //    in = [ "dd", "d", "aa", "ww", "qq"]
 //    out = ["dd", "d"]
 
-    public static int [] halfOfArray(int[] num) {
+    public static int[] halfOfArray(int[] num) {
         int len = num.length;
         int halfLen = len / 2;
         int result[] = new int[halfLen];
@@ -39,8 +70,18 @@ public class Runner {
         return result;
     }
 
+    public static String[] halfOfStringArray(String[] letters) {
+        int length = letters.length;
+        int halflength = length / 2;
+        String result[] = new String[halflength];
+        for (int i = 0; i < halflength; i++) {
+            result[i] = letters[i];
+        }
+        return result;
+    }
 
-//    инпут - aabbba
+
+ //   инпут - aabbba
 //    output - 2a3b1a
     public static void archiveInfo(String letter) {
 

29d5cb7936ebf1607196fa64a398d7f958e83260
nero <sergeinerobeev@gmail.com>
2024-06-28 16:45:59 +0200

    doing

--- src/leetcode/arrays/ShuffleTheArray.java
+++ src/leetcode/arrays/ShuffleTheArray.java
@@ -12,7 +12,8 @@ public class ShuffleTheArray {
 
     public static int[] shuffle(int[] nums, int n) {
         int temp = 0;
-        for (int i = 1; i < nums.length - 1 / n; i++) {
+        int [] result = new int[nums.length];
+        for (int i = 1; i < nums.length; i++) {
             temp = nums[i + 1];
             nums[i + 1] = nums[i + 2];
             nums[i + 2] = temp;

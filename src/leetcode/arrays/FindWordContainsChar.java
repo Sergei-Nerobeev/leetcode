4eff998303e96b0998bca39e05d8bd5424368d18
Sergei Nerobeev <sergeinerobeev@gmail.com>
2024-07-26 22:36:12 +0200

    FindWordContainsChar

--- src/leetcode/arrays/FindWordContainsChar.java
+++ src/leetcode/arrays/FindWordContainsChar.java
@@ -1,14 +1,23 @@
 package leetcode.arrays;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FindWordContainsChar {
     public static void main(String[] args) {
 
     }
-    public List<Integer> findWordsContaining(String[] words, char x) {
+
+    public static List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> result = new ArrayList<>();
         for (int i = 0; i < words.length; i++) {
-           words[i].toCharArray().
+            for (char c : words[i].toCharArray()) {
+                if (c == x) {
+                    result.add(i);
+                    break;
+                }
+            }
         }
+        return result;
     }
 }

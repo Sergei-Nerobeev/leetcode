a81fbc7304dc4b4c86c9e04d21a5980cdc07d8b2
nero <sergeinerobeev@gmail.com>
2024-07-29 18:34:53 +0200

    fibonacci number

---
+++ src/leetcode/recurs/FibonacciNumber.java
@@ -0,0 +1,11 @@
+package leetcode.recurs;
+
+public class FibonacciNumber {
+    public static void main(String[] args) {
+        System.out.println(fib(2));
+    }
+    public static int fib(int n){
+
+    return 0;
+    }
+}

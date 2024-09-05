cc81c5ff08844a8fa681d298b360dd99a632409e
nero <sergeinerobeev@gmail.com>
2024-09-05 17:44:13 +0200

    start linkedArray

--- src/leetcode/my/myarrays/Main.java
+++ src/leetcode/my/myarrays/Main.java
@@ -6,18 +6,26 @@ public class Main {
         var head = new Patient(null, "Bob");
         var one = new Patient(head, "Nikola");
         var second = new Patient(one, "Lisa");
-        var me = new Patient(null, "Me");
+        var me = new Patient(second, "Me");
 
-        printIt(second);
-        paste(one, me.name);
-        printIt(second);
-        paste(second, me.name);
-        printIt(second);
+        insert(head, "Nikola", "Me");
+        printIt(head);
+        System.out.println("-----------------------");
+        printIt(head);
     }
 
-    public static void paste(Patient patient, String name) {
-        Patient newPatient = new Patient(patient.previous, name);
-        patient.previous = newPatient;
+    public static void insert(Patient head, String targetName, String newName) {
+        Patient current = head;
+        while (current != null) {
+            if (current.name.equals(targetName)) {
+                Patient newPatient = new Patient(current.previous, newName);
+                if (current.previous != null) {
+                    current.previous = newPatient;
+                    break;
+                }
+            }
+            current = current.previous;
+        }
     }
 
     public static void printIt(Patient patient) {

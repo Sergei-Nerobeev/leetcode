605fe06dc267f12845832341a77c9393b72ea704
Sergei Nerobeev <sergeinerobeev@gmail.com>
2024-09-19 17:43:54 +0200

    sort

--- src/leetcode/my/myarrays/reversInt/Main.java
+++ src/leetcode/my/myarrays/reversInt/Main.java
@@ -1,13 +1,15 @@
 package leetcode.my.myarrays.reversInt;
 
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
 
-        int[] array = {2, 1, 3, 5, 4};
-//        System.out.println(Arrays.toString(getReversInt(array)));
-        System.out.println(Arrays.toString(getSorted(array)));
+        int[] array = {2, 9, 3};
+
+        sort(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
 
     }
 
@@ -19,14 +21,16 @@ public class Main {
         return reversArray;
     }
 
-    public static int[] getSorted(int[] array) {
-        int[] sortedArray = new int[array.length];
-        int temp = -1;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] > array[i + 1]) {
-                temp = array[i];
-                sortedArray[i] = temp;
-            }
-        } return sortedArray;
+    public static void sort(int[] array) {
+        // внешний цикл проходит по массиву
+        for (int i = 0; i < array.length; i++) { // i = 3, 3  < 3,  {2,3,9}
+            // внутренний цикл сравнивает и меняет местами
+            for (int j = 1; j < array.length - i; j++) // j = 1, 1 < 1, {2,3,9}
+                if (array[j] < array[j - 1]) { // 3 < 2 ,
+                    int temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+                }
+        }
     }
 }

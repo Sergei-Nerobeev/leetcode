c8d8910182e26b48ec176aa6e0a84631c66f700a
Sergei Nerobeev <sergeinerobeev@gmail.com>
2024-09-23 20:47:00 +0200

    sort done

--- src/leetcode/my/myarrays/reversInt/Main.java
+++ src/leetcode/my/myarrays/reversInt/Main.java
@@ -1,5 +1,6 @@
 package leetcode.my.myarrays.reversInt;
 
+
 public class Main {
     public static void main(String[] args) {
 
@@ -8,9 +9,7 @@ public class Main {
         sort(array);
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
-
         }
-
     }
 
     public static int[] getReversInt(int[] array) {
@@ -25,7 +24,7 @@ public class Main {
         // внешний цикл проходит по массиву
         for (int i = 0; i < array.length; i++) { // i = 3, 3  < 3,  {2,3,9}
             // внутренний цикл сравнивает и меняет местами
-            for (int j = 1; j < array.length - i; j++) // j = 1, 1 < 1, {2,3,9}
+            for (int j = 1; j < array.length; j++) // j = 1, 1 < 1, {2,3,9}
                 if (array[j] < array[j - 1]) { // 3 < 2 ,
                     int temp = array[j];
                     array[j] = array[j - 1];

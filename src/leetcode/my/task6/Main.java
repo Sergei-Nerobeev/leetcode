57d5aafb40062d3fa096166f29fd9b714fe8d1e1
Sergei Nerobeev <sergeinerobeev@gmail.com>
2025-03-17 22:11:03 +0100

    task 6 Main.java commit

---
+++ src/leetcode/my/task6/Main.java
@@ -0,0 +1,30 @@
+package leetcode.my.task6;
+
+public class Main {
+    public static void main(String[] args) {
+//        У вас есть переменные x1, x2, x3, которые содержат входные пользовательские данные.
+//        Напишите код, который находит максимальное и минимальное число из x1, x2, x3 и записывает результат в переменную result.
+//        Sample Input 1:
+//        2 1 3
+//        Sample Output 1:
+//        минимальное: 1, максимальное: 3
+//        Sample Input 2:
+//        2 4 3
+//        Sample Output 2:
+//        минимальное: 2, максимальное: 4
+
+
+//    }
+//
+//    public static int findMaxAndMinValue(int x1, int x2, int x3) {
+//
+//        int min = x1;
+//        int max = x2;
+//        if(x2 < min && x3 < min) {
+//
+//        }
+//
+//
+    }
+
+}

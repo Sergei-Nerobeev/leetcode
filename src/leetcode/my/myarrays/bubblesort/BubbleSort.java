75b8bf17f09003ad39eddcd69ebcd5559bd377c1
coder <sergeinerobeev@gmail.com>
2025-05-21 16:10:17 +0200

    added BubbleSort.java

---
+++ src/leetcode/my/myarrays/bubblesort/BubbleSort.java
@@ -0,0 +1,32 @@
+package leetcode.my.myarrays.bubblesort;
+
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+public class BubbleSort {
+  public static void main(String[] args) {
+    int[] numbers = {120, 70, 5, 800,5890};
+    System.out.println(Arrays.toString(bubbleSort(numbers)));
+  }
+
+  public static int[] bubbleSort(int[] numbers) {
+
+    var isEmpty = numbers.length;
+    if (isEmpty == 0) {
+      throw new NoSuchElementException("Array is empty");
+    }
+    for (int i = 0; i < numbers.length; i++) { // outer loop
+      for (int j = 0; j < numbers.length - i - 1; j++) { // inner loop
+        if (numbers[j] > numbers[j + 1]) {
+          int temp = numbers[j];
+          numbers[j] = numbers[j + 1];
+          numbers[j + 1] = temp;
+        }
+      }
+    }
+    return numbers;
+  }
+
+}
+
+

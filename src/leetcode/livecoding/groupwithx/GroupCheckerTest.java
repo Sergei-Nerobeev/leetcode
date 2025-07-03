503a24a0e851b332b323a34bde1b23abe487f6b3
coder <sergeinerobeev@gmail.com>
2025-07-03 18:15:54 +0200

    added test with Group is null.java

--- src/leetcode/livecoding/groupwithx/GroupCheckerTest.java
+++ src/leetcode/livecoding/groupwithx/GroupCheckerTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@DisplayName("GroupChecker Test")
+@DisplayName("GroupChecker method Test")
 class GroupCheckerTest {
 
   List<Group> groups = new ArrayList<>();
